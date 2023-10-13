e2f291e5619b09d0bd890eed4c20f0d10cf01b51
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-10-12 23:40:23 -0400

    Create tips.php

---
+++ pages/tips.php
@@ -0,0 +1,84 @@
+<?php
+require_once('../lib/functions.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="../foot_in_door_website/assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="../foot_in_door_website/css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed-fill" viewBox="0 0 16 16">
+				<path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+				</svg><a class="navbar-brand" href="#!">Foot In Door</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../foot_in_door_website/index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../pages/create_resume.php">Create Resume</a></li>
+						<li class="nav-item"><a class="nav-link" href="../pages/templates.php">Templates</a></li>
+						<li class="nav-item"><a class="nav-link" href="../pages/tips.php">Resume-Making Tips</a></li>
+						<li class="nav-item"><a class="nav-link" href="../pages/discussion_board.php">Discussion Board</a></li>
+						<li class="nav-item"><a class="nav-link" href="../pages/faq.php">FAQ/Support</a></li>
+						<li class="nav-item"><a class="nav-link" href="../pages/contact_us.php">Contact Us</a></li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" >
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+						<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+						<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z"/>
+						</svg>
+						<?php
+						if(isset($_SESSION['username']) && isset($_SESSION['password'])){ ?>
+							<a class="nav-link" href="../pages/user_profile.php">Go To Profile</a>
+						<?php } else { ?>
+							<a class="nav-link" href="../pages/signup_or_login.php">Signup/Login</a>
+						<?php } ?>
+                            
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+		<!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Important Tips</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Make sure to include these on your resume!</p>
+                </div>
+            </div>
+        </header>
+		
+		<ul>
+			<li>Include name, address, email, and phone number. Also include linkedin username and website url if you have one.</li>
+			<li>Include a summary about yourself and what you hope to achieve if you get this new job</li>
+			<li>Include work history including job title, company worked for, location, and job description. Also consider including achievements gained from the job.</li>
+			<li>Include previously and/or currently attended schools.</li>
+			<li>Include key skills you have that would benefit the company you are applying for</li>
+			<li>Consider including favorite hobbies. Some employers like to see what you are like outside of work.</li>
+		</ul>
+
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Foot in Door 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="../foot_in_door_website/js/scripts.js"></script>
+	</body>
+</html>
