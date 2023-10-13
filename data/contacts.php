813931deea3e3876499698f9009c937f95b482d9
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-10-13 00:33:13 -0400

    Update contacts.php

--- data/contacts.php
+++ data/contacts.php
@@ -4,28 +4,28 @@ $contacts=
 	'Evan McMillin'=>
 	[
 		'name'=>'Evan McMillin',
-		'picture'=>'me_lancer1.jpg',
+		'picture'=>'../foot_in_door_website/assets/me_lancer1.jpg',
 		'email'=>'mcmilline1@nku.edu',
 		'phone'=>'(513)869-9060'
 	],
 	'Julianna Truitt'=>
 	[
 		'name'=>'Julianna Truitt',
-		'picture'=>'julianna.jpg',
+		'picture'=>'../foot_in_door_website/assets/julianna.jpg',
 		'email' =>'truittj3@nku.edu',
 		'phone'=>'859-609-5761'
 	],
 	'Leo Foy'=>
 	[
 		'name'=>'Leo Foy',
-		'picture'=>'leo.jpg',
+		'picture'=>'../foot_in_door_website/assets/leo.jpg',
 		'email' =>'lfoy306@gmail.com',
 		'phone'=>'(802)622-4425'
 	],
 	'Andrew Keller'=>
 	[
 		'name'=>'Andrew Keller',
-		'picture'=>'andrew.jpg',
+		'picture'=>'../foot_in_door_website/assets/andrew.jpg',
 		'email' =>'kellera11@mymail.nku.edu',
 		'phone'=>'513 555 5855'
 	],
