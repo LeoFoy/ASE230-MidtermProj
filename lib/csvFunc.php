4a8e88981e21790e7aeffa71acfb0afa22a3f6b2
juliannatruitt <143114521+juliannatruitt@users.noreply.github.com>
2023-10-15 10:33:30 -0400

    Update csvFunc.php
    Adding functions file to array with four indexes and file to array with one index

--- lib/csvFunc.php
+++ lib/csvFunc.php
@@ -1,5 +1,18 @@
 <?php
 
+function csvFiletoArrayWithOneIndexes($path){
+	$csvArray = [];
+	$fp = fopen($path, 'r');
+		while (!feof($fp)){
+			$line = fgets($fp);
+			if(strlen($line) < 5) continue;
+				array_push($csvArray, [$line]);
+		
+		}
+		fclose($fp);	
+		return $csvArray;
+}
+
 function csvFiletoArrayWithTwoIndexes($path){
 	$csvArray = [];
 	$fp = fopen($path, 'r');
@@ -22,6 +35,23 @@ function csvFiletoArrayWithThreeIndexes($path){
 			if(strlen($line) < 5) continue;
 				$line = explode(';', trim($line));
 				array_push($csvArray, [$line[0], $line[1], $line[2]]);
+		
+		}
+		fclose($fp);	
+		return $csvArray;
+}
+
+//BE AWARE:
+//this function explodes a line with a ',' instead of a ';' like the previous functions ^
+//this is because this is how the users.csv.php file is set up.
+function csvFiletoArrayWithFourIndexes($path){
+	$csvArray = [];
+	$fp = fopen($path, 'r');
+		while (!feof($fp)){
+			$line = fgets($fp);
+			if(strstr($line, '<?php die() ?>') || strlen($line) < 5) continue;
+				$line = explode(',', trim($line));
+				array_push($csvArray, [$line[0], $line[1], $line[2], $line[3]]);
 		
 		}
 		fclose($fp);	
