11fff67330348e9e62d9a36084aa20eb3fbebfad
LeoFoy <lfoy306@gmail.com>
2023-10-15 22:20:30 -0400

    Fixing nav bar paths

--- pages/discussion_board/discussion_board.php
+++ pages/discussion_board/discussion_board.php
@@ -27,10 +27,10 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Create Resume</a></li>
-						<li class="nav-item"><a class="nav-link" href="#!">Discussion Board</a></li>
-						<li class="nav-item"><a class="nav-link" href="#!">Contact Us</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../../foot_in_door_website/index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../../create_resume.php">Create Resume</a></li>
+						<li class="nav-item"><a class="nav-link" href="discussion_board.php">Discussion Board</a></li>
+						<li class="nav-item"><a class="nav-link" href="../contact_us.php">Contact Us</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Templates</a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
